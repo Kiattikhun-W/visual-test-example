44568c140bfdc2f0ac3bedf1795d2aaf6a3c4b4a
Kiadtikhun <kiadtikhun.w@gmail.com>
2024-02-27 18:09:30 +0700

    Add visual tests using webdriver.io and chai

--- src/AppTest.ts
+++ src/AppTest.ts
@@ -1,23 +1,55 @@
 import { remote } from "webdriverio";
 import { checkScreenshots } from "./Utils/visual-test.js";
+import { assert } from "chai";
 
+import { describe, it } from "mocha";
 async function AppTest() {
-  const browser = await remote({
+  return await remote({
     logLevel: "silent",
     capabilities: {
       browserName: "chrome",
     },
   });
-
-  await browser.url("https://webdriver.io/");
-  await checkScreenshots(
-    {
-      selector: ".hero__title",
-      // selector: "#ms-floating-button",
-      filename: "test2",
-    },
-    browser
-  );
-  await browser.deleteSession();
 }
-AppTest();
+const browser = AppTest();
+
+describe("webdriver.io page", function () {
+  after(async () => {
+    await (await browser).closeWindow();
+    await (await browser).deleteSession();
+  });
+  it("should be return true because img is static", async () => {
+    await (await browser).url("https://webdriver.io/");
+    const { result, message, numDiffPixels } = await checkScreenshots(
+      {
+        selector: `a.button[href="/docs/gettingstarted"]`,
+        // selector: "#ms-floating-button",
+        filename: "test2",
+      },
+      await browser
+    );
+    console.log(
+      `result: ${result}, message: ${message}, numDiffPixels: ${numDiffPixels}`
+    );
+
+    assert.isTrue(result);
+  });
+
+  it("should be return false because different img", async () => {
+    await (await browser).url("https://webdriver.io/");
+    const { result, message, numDiffPixels } = await checkScreenshots(
+      {
+        // selector: `a.button[href="/docs/gettingstarted"]`,
+        selector: "#ms-floating-button",
+        filename: "test2",
+      },
+      await browser
+    );
+
+    console.log(
+      `result: ${result}, message: ${message}, numDiffPixels: ${numDiffPixels}`
+    );
+
+    assert.isFalse(result);
+  });
+});
