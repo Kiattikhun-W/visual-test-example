312aa287ba8fac4e46b31836bbf0f9ecfaa9fd4c
Kiadtikhun <kiadtikhun.w@gmail.com>
2024-02-28 13:39:18 +0700

    Add Options interface for element selector and screenshot filename

--- src/types/type.ts
+++ src/types/type.ts
@@ -5,8 +5,17 @@ type Brand<Type, Key> = Type & { _brand: Key };
 export type AppType = Brand<string, "apptype">;
 export type PathWithPNG = `${string}.png`;
 
+/**
+ * Represents the element selector and the filename for the screenshot.
+ */
 export interface Options {
+  /**
+   * selector - The element selector for the screenshot.
+   */
   selector: string;
+  /**
+   * filename - The title for the screenshot, We'll use this for compare image as the same title.
+   */
   filename: string;
   frame?: string;
 }
